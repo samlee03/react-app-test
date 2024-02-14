280535fefef604c0cd51a45c959bb22d1a265615
Sam <sam.lee03@myhunter.cuny.edu>
2024-02-13 21:48:55 -0500

    initial commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,24 @@
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
+import Food from './Food.jsx'
+import Card from './Card.jsx'
+
+function App() {
+  return(
+    <>
+      <Header></Header>
+      <Food/>
+      <Card/>
+      <Card/>
+
+      <Card/>
+      <Card/>
+
+
+      <Footer></Footer>
+    </>
+  );
+
+}
+
+export default App
---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,10 @@
+function Card(){
+    return (
+        <div className="card">
+            <img className="card-image" alt="profile picture" src="https://via.placeholder.com/150"></img>
+            <h2 className="card-title">Sam</h2>
+            <p className="card-text">I go to school and I play videogames.</p>
+        </div>
+    );
+}
+export default Card
