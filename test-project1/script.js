88e52fee216580d178bd543ae04c9422c189e584
Sam <sam.lee03@myhunter.cuny.edu>
2024-03-08 21:16:24 -0500

    added shuffling

--- test-project1/script.js
+++ test-project1/script.js
@@ -1,21 +1,66 @@
-console.log("This is my first line of code");
 var startButton = document.getElementById("start-button");
 var body = document.getElementById("body");
+var lifeDisplay = document.getElementById("life-display");
+var title = document.getElementById("title");
+var stageDisplay = document.getElementById("stage-display");
 
-function wipeElements() {
-    var body = document.getElementById('body');
-
-    while(body[0].className != "lives") {
-        body[0].removeChild(body[0]);
-    }
-}
+var startElements = [startButton, lifeDisplay, title];
 var lives = 3;
 var stage = 0;
-function makeElement(elementName, className, n){
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function wipeButtons() {
+    var allButtons = document.getElementsByTagName("button");
+    while (allButtons[0]) {
+        allButtons[0].remove();
+    }
+}
+
+function decrementLife(){
+    lives = lives - 1;
+    lifeDisplay.innerHTML = ("lives left: " + lives);
+    switch (lives){
+        case 2:
+            lifeDisplay.style.color = "#c91c36";
+            break;
+        case 1:
+            lifeDisplay.style.color = "#ff0026";
+            break;
+        case 0:
+            wipeButtons();
+            var myText = document.createElement("h2");
+            myText.innerHTML = "no more lives available";
+            body.appendChild(myText);
+    }
+}
+
+function makeElement(elementName, className, n, elementText){
     for (let i = 0; i < n; i++){
         var myElement = document.createElement(elementName);
+        myElement.innerHTML = elementText;
         myElement.classList.add(className);
+        if (className == "fake-button"){
+            myElement.addEventListener("click", decrementLife);
+        }
+        if (className == "real-button"){
+            myElement.addEventListener("click", incrementStage);
+        }
+        
         body.appendChild(myElement);
+    }
+}
+function shuffle(amt){
+    var myNumber = getRandomInt(amt);
+    for (let i = 0; i < amt; i++){
+        if (i == myNumber){
+            makeElement("button", "real-button", 1, "");
+        }
+        else {
+            makeElement("button", "fake-button", 1, "");
+        }
     }
 }
 function incrementStage(){
@@ -24,9 +69,11 @@ function incrementStage(){
     startButton.remove();
     switch(stage){
         case(1):
-            makeElement("button", "fake-button", 3);
-            makeElement("button", "real-button", 1);
-            makeElement("button", "fake-button", 2);
+            stageDisplay.innerHTML = ("current stage: " + stage);
+            shuffle(10);
+            break;
+        case(2):
+            wipeButtons();
     }
 }
 // while (stage < 2){
