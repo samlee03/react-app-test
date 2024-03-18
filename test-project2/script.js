70e387ef94163895212e5d15bd293865a3a1e003
Sam <sam.lee03@myhunter.cuny.edu>
2024-03-17 21:37:40 -0400

    add user input

--- test-project2/script.js
+++ test-project2/script.js
@@ -3,6 +3,7 @@ var playButton = document.getElementById("play");
 var isPlayingSequence = false;
 var sequence = [];
 var playerSequence = [];
+var score = 0;
 
 // Helper Functions
 function getRandomInt(max) {
@@ -14,23 +15,10 @@ function wait(ms) {
 /* *** */
 
 function activateButton(buttonElement){
-    // switch (buttonId){
-    //     case (1):
-    //         console.log("activated button");
-    //         buttonElement.style.backgroundColor = "black";
-    //         break;
-    //     default:
-    //         break;
-    // }
     buttonElement.style.backgroundColor = "black";
 }
 
 function resetButton(buttonElement, buttonId){
-    // switch (buttonId){
-    //     case (1):
-    //         console.log("reset button");
-    //         buttonElement.style.backgroundColor = "";
-    // }
     buttonElement.style.backgroundColor = "";
 }
 function animateButton(buttonElement) {
@@ -40,12 +28,26 @@ function animateButton(buttonElement) {
     activateButton(buttonElement);
     setTimeout(() => {
         resetButton(buttonElement);
-    }, 750);
+    }, 500);
 }
-function playingSequence(){
+/* Check for Correctness */
+function checkInSequence1() {
+    checkInSequence(1);
+}
+function checkInSequence2() {
+    checkInSequence(2);
+}
+function checkInSequence3() {
+    checkInSequence(3);
+}
+
+function playingSequence(score){
     if (!isPlayingSequence){
-        playSequence(3, 3);
-        playButton.removeEventListener('click', playingSequence);
+        playSequence(3 + score, 3);
+        playButton.removeEventListener('click', () => playingSequence(score));
+        b1.removeEventListener("click", checkInSequence1);
+        b2.removeEventListener("click", checkInSequence2);
+        b3.removeEventListener("click", checkInSequence3);
     }
 }
 function playSequence(seqAmt, buttonAmt){
@@ -55,17 +57,83 @@ function playSequence(seqAmt, buttonAmt){
             var randNum = getRandomInt(buttonAmt) + 1;
             console.log(randNum);
             sequence[i] = randNum;
-            setTimeout(() => animateButton(allButtons[randNum-1]), index * 1250);
+            setTimeout(() => animateButton(allButtons[randNum-1]), index * 1000);
         })(i);
     }
     setTimeout(function(){
         isPlayingSequence = false;
-        playButton.addEventListener('click', playingSequence);
+        playButton.addEventListener('click', () => playingSequence(score));
+        b1.addEventListener("click", checkInSequence1);
+        b2.addEventListener("click", checkInSequence2);
+        b3.addEventListener("click", checkInSequence3);
         console.log("Sequence: " + sequence);
-    }, seqAmt * 1250)
+    }, seqAmt * 1000)
 }
 
 
 // playButton.addEventListener('click', () => activateButton(allButtons[0], 1));
-playButton.addEventListener('click', playingSequence);
+playButton.addEventListener('click', () => playingSequence(score));
 
+var b1 = document.getElementById("b1");
+var b2 = document.getElementById("b2");
+var b3 = document.getElementById("b3");
+function lightUpButton(buttonPressed, isCorrect){
+    switch (buttonPressed) {
+        case (1):
+            isCorrect? b1.style.backgroundColor = "green" : b1.style.backgroundColor = "red";
+            if (!isCorrect){
+                return;
+            }
+            setTimeout(function(){
+                b1.style.backgroundColor = "";
+            }, 500);
+            break;
+        case (2):
+            isCorrect? b2.style.backgroundColor = "green" : b2.style.backgroundColor = "red";
+            if (!isCorrect){
+                return;
+            }
+            setTimeout(function(){
+                b2.style.backgroundColor = "";
+            }, 500);
+            break;
+        case (3):
+            isCorrect? b3.style.backgroundColor = "green" : b3.style.backgroundColor = "red";
+            if (!isCorrect){
+                return;
+            }
+            setTimeout(function(){
+                b3.style.backgroundColor = "";
+            }, 500);
+            break;
+    }
+}
+function checkInSequence(buttonPressed){
+    playerSequence.push(buttonPressed);
+    console.log(playerSequence.length);
+    for (let i = 0; i < playerSequence.length; i++){
+        if (sequence[i] != playerSequence[i]){
+            console.log("WRONG ANSWER");
+            playerSequence = [];
+            lightUpButton(buttonPressed, false);
+            return;
+        } else {
+            lightUpButton(buttonPressed, true);
+        }
+    }
+    if (playerSequence.length == sequence.length){
+        console.log("SEQUENCE COMPLETED +1");
+        score++;
+        var scoreDisplay = document.getElementById("score");
+        scoreDisplay.innerHTML = ("score: " + score);
+        playerSequence = [];
+        b1.removeEventListener("click", checkInSequence1);
+        b2.removeEventListener("click", checkInSequence2);
+        b3.removeEventListener("click", checkInSequence3);
+        setTimeout(() => playingSequence(score), 1000);
+    } 
+}
+
+// b1.addEventListener("click", () => checkInSequence(1));
+// b2.addEventListener("click", () => checkInSequence(2));
+// b3.addEventListener("click", () => checkInSequence(3));
